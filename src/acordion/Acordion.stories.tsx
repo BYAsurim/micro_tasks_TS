d41fc10d4250fbf0e1efbeeabc782036bbb5286a
BYAsurim <Pers0naL276276@gmail.com>
2023-09-13 22:10:33 +0300

    added Acordion.stories.tsx

---
+++ src/acordion/Acordion.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta } from '@storybook/react';
+
+import { Accordion } from './Accordion';
+
+const meta: Meta<typeof Accordion> = {
+    component: Accordion,
+};
+
+export default meta;
+
+export const CollapsedAccordion = () => {
+    return <div>hello</div>
+}
+
+export const OpenAccordion = () => {
+    return <div>hello</div>
+}
